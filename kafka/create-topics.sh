6a51c8b293f24b5f222bc34a5750c1fe5ae5c07e
seyed amir mohammad sadat shokouhi <sam.sadat.shokouhi@gmail.com>
2022-09-03 02:20:38 +0430

    fix create-topics.sh

--- kafka/create-topics.sh
+++ kafka/create-topics.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka1:9092,kafka1:9092 --replication-factor 3 --partitions 10 --topic events_by_user_id
-docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka1:9092,kafka1:9092 --replication-factor 3 --partitions 10 --topic events_by_course_id
-docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka1:9092,kafka1:9092 --replication-factor 3 --partitions 10 --topic events_by_user_id_course_id
-docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka1:9092,kafka1:9092 --replication-factor 3 --partitions 10 --topic video_events_by_user_id_course_id
+docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --replication-factor 3 --partitions 10 --topic events_by_user_id
+docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --replication-factor 3 --partitions 10 --topic events_by_course_id
+docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --replication-factor 3 --partitions 10 --topic events_by_user_id_course_id
+docker run --network story-network bitnami/kafka:3.2.1 kafka-topics.sh --create --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --replication-factor 3 --partitions 10 --topic video_events_by_user_id_course_id
